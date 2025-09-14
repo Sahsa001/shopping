b826ffd2a18b1c6ae2e18c3632e0f6a8f634d743
Sahsa001 <abdumalikovvv6gmail.com>
2025-09-14 12:58:20 +0600

    fix6

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,8 @@
 
 import React from "react";
-import Header from "./header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer/Footer";
+import Header from "./header/Header";
 
 function Layout() {
   return (
